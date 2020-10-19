920b17ae52305d91a972853389c96871235753b3
ADoessing <32567122+ADoessing@users.noreply.github.com>
2020-10-19 14:59:05 +0200

    Changes

--- Program.cs
+++ Program.cs
@@ -1,5 +1,8 @@
-﻿using Microsoft.ML;
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.ML;
+using Microsoft.ML.Data;
 using System;
+using System.Data.SqlClient;
 
 namespace temperaturepredictor
 {
@@ -7,12 +10,36 @@ namespace temperaturepredictor
     {
         static void Main(string[] args)
         {
-
+            DdHelper ddHelper = new DdHelper();
             var context = new MLContext();
 
+            var builder = new ConfigurationBuilder();
+
+            var configuration = builder.Build();
+            var connectionString = ddHelper.GetConnectionString();
+            var columnLoader = new DatabaseLoader.Column[]
+            {
+                new DatabaseLoader.Column() {Name="Stores",Type=System.Data.DbType.Int32},
+                new DatabaseLoader.Column() {Name="Alarms",Type=System.Data.DbType.Int32},
+                new DatabaseLoader.Column() {Name="TempMean",Type=System.Data.DbType.Decimal},
+                new DatabaseLoader.Column() {Name="Humidity",Type=System.Data.DbType.Int32},
+                new DatabaseLoader.Column() {Name="Pressure",Type=System.Data.DbType.Decimal},
+                new DatabaseLoader.Column() {Name="TempMin",Type=System.Data.DbType.Decimal},
+                new DatabaseLoader.Column() {Name="TempMax",Type=System.Data.DbType.Decimal}
+
+            };
+
+            var connection = new SqlConnection(connectionString);
+            var factory = DbProviderFactories.GetFactory(connection);
+            var loader = context.Data.CreateDatabaseLoader(columnLoader);
+
+            var dbSource = new DatabaseSource(factory, connectionString, "");
+
+            var trainData = loader.Load(dbSource);
+
             //Load data
-            var trainData = context.Data.LoadFromTextFile<TemperatureData>(@"C:\Users\Asmus\source\repos\temperaturepredictor\AlarmDataTestAllStations.csv",
-                hasHeader: true, separatorChar: ',');
+            //var trainData = context.Data.LoadFromTextFile<TemperatureData>(@"C:\Users\Asmus\source\repos\temperaturepredictor\AlarmDataTestAllStations.csv",
+            //    hasHeader: true, separatorChar: ',');
 
             //splits data into test and train sets.
             var testTrainSplit = context.Data.TrainTestSplit(trainData, testFraction: 0.30);
