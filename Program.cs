85801cca073655864637a0881c54a05ca70b1ace
ADoessing <32567122+ADoessing@users.noreply.github.com>
2020-11-11 13:53:26 +0100

    Smoll update 2

--- Program.cs
+++ Program.cs
@@ -28,16 +28,16 @@ namespace temperaturepredictor
             //    trainedModel = context.Model.Load(modelFile, out modelSchema);
             //}
 
-            //var trainData = context.Data.LoadFromTextFile<TemperatureData>(@"C:\Users\Asmus\Source\Repos\ADoessing\AlarmPressureEstimator\Csvs\AlarmDataTestAllStationsPerfect.csv",
-            //    hasHeader: true, separatorChar: ',');
+            var trainData = context.Data.LoadFromTextFile<TemperatureData>(@"C:\Users\Asmus\source\repos\temperaturepredictor\Csvs\FixedDataTestAll(3)2.csv",
+                hasHeader: true, separatorChar: ',');
 
-            ////splits data into test and train sets.
-            //var testTrainSplit = context.Data.TrainTestSplit(trainData, testFraction: 0.30);
-            //// build the model
-            //var pipeline = context.Transforms.Concatenate("Features", new[] { "Stores", "AlarmItems", "TempMean", "Humidity", "Pressure", "TempMin", "TempMax" })
-            //    .Append(context.Regression.Trainers.FastTree());
+            //splits data into test and train sets.
+            var testTrainSplit = context.Data.TrainTestSplit(trainData, testFraction: 0.30);
+            // build the model
+            var pipeline = context.Transforms.Concatenate("Features", new[] { "Stores", "AlarmItems", "TempMean", "Humidity", "Pressure", "TempMin", "TempMax" })
+                .Append(context.Regression.Trainers.FastTree());
 
-            //var model = pipeline.Fit(testTrainSplit.TrainSet);
+            var model = pipeline.Fit(testTrainSplit.TrainSet);
 
             //context.Model.Save(model, trainData.Schema, "model.zip");
 
@@ -63,32 +63,32 @@ namespace temperaturepredictor
                 TempMin = 9.6F,
                 TempMax = 17.2F
             };
-            //var predictionFunc = context.Model.CreatePredictionEngine<TemperatureData, AlarmPressurePrediction>(trainedModel);
+            var predictionFunc = context.Model.CreatePredictionEngine<TemperatureData, AlarmPressurePrediction>(model);
 
-            //var prediction = predictionFunc.Predict(newData);
+            var prediction = predictionFunc.Predict(newData);
 
             //DdHelper ddHelper = new DdHelper();
-            ddHelper.SavePerfectAlarmDataset3();
+            //ddHelper.SavePerfectAlarmDataset3();
 
             //var values = ddHelper.GetNewestWeatherforecastByStationId("06056", 1);
             //Console.WriteLine(values.Item2.Substring(6, 4) + "-" + (Convert.ToInt32(values.Item2.ToString().Substring(3, 2)) + 1).ToString() + "-" + values.Item2.ToString().Substring(0, 2));
 
-            //List<string> Stations = ddHelper.GetStations();
-            //for (int i = 0; i < Stations.Count; i++)
-            //{
-            //    for (int j = 1; j <= 7; j++)
-            //    {
-            //        var values = ddHelper.GetNewestWeatherforecastByStationId(Stations[i], j);
-            //        prediction = predictionFunc.Predict(values.Item1);
-            //        var date = values.Item2.Substring(6, 4) + "-" + values.Item2.ToString().Substring(3, 2) + "-" + (Convert.ToInt32(values.Item2.ToString().Substring(0, 2)) + j).ToString();
-            //        Console.WriteLine(values.Item2.Substring(6, 4) + "-" + (Convert.ToInt32(values.Item2.ToString().Substring(0, 2)) + j).ToString() + "-" + values.Item2.ToString().Substring(3, 2));
-            //        ddHelper.UploadNewestAlarmPrediction(date, Stations[i], Convert.ToInt32(prediction.PredictedAlarmPressure));
-            //    }
-            //}
+            List<string> Stations = ddHelper.GetStations();
+            for (int i = 0; i < Stations.Count; i++)
+            {
+                for (int j = 1; j <= 7; j++)
+                {
+                    var values = ddHelper.GetNewestWeatherforecastByStationId(Stations[i], j);
+                    prediction = predictionFunc.Predict(values.Item1);
+                    var date = values.Item2.Substring(6, 4) + "-" + values.Item2.ToString().Substring(3, 2) + "-" + (Convert.ToInt32(values.Item2.ToString().Substring(0, 2)) + j).ToString();
+                    Console.WriteLine(values.Item2.Substring(6, 4) + "-" + (Convert.ToInt32(values.Item2.ToString().Substring(0, 2)) + j).ToString() + "-" + values.Item2.ToString().Substring(3, 2));
+                    ddHelper.UploadNewestAlarmPrediction(date, Stations[i], Convert.ToInt32(prediction.PredictedAlarmPressure));
+                }
+            }
 
-            Console.WriteLine("Færdig");
-            //Console.WriteLine($"Prediction - {prediction.PredictedAlarmPressure}");
-            //Console.ReadLine();
+            //Console.WriteLine("Færdig");
+            Console.WriteLine($"Prediction - {prediction.PredictedAlarmPressure}");
+            Console.ReadLine();
 
         }
 
