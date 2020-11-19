ddb8f900c86e447b7ddb8011d962579579ebc7f6
ADoessing <32567122+ADoessing@users.noreply.github.com>
2020-11-19 19:33:58 +0100

    Yes

--- Program.cs
+++ Program.cs
@@ -3,6 +3,7 @@ using Microsoft.ML;
 using Microsoft.ML.Data;
 using System;
 using System.Collections.Generic;
+using System.Collections.Immutable;
 using System.Data.Common;
 using System.Data.SqlClient;
 using System.IO;
@@ -35,7 +36,7 @@ namespace temperaturepredictor
             var model = pipeline.Fit(testTrainSplit.TrainSet);
 
             //evaluate
-            StreamWriter writer = new StreamWriter(@"C:\Users\Asmus\Source\Repos\ADoessing\AlarmPressureEstimator\Csvs\results.txt", true);
+            //StreamWriter writer = new StreamWriter(@"C:\Users\Asmus\Source\Repos\ADoessing\AlarmPressureEstimator\Csvs\results.txt", true);
             var predictions = model.Transform(testTrainSplit.TestSet);
 
             var metrics = context.Regression.Evaluate(predictions);
@@ -48,17 +49,18 @@ namespace temperaturepredictor
             //predict
             var newData = new TemperatureData
             {
-                Stores = 55F,
-                AlarmItems = 1607F,
-                TempMean = 13.10F,
-                Humidity = 83.541666F,
-                Pressure = 1025.062500F,
-                TempMin = 9.6F,
-                TempMax = 17.2F
+                Stores = Convert.ToSingle(84),
+                AlarmItems = Convert.ToSingle(2301),
+                TempMean = Convert.ToSingle(10.262500),
+                Humidity = Convert.ToSingle(94.791666),
+                Pressure = Convert.ToSingle(1015.554166),
+                TempMin = Convert.ToSingle(8.4000),
+                TempMax = Convert.ToSingle(12.1000)
             };
             var predictionFunc = context.Model.CreatePredictionEngine<TemperatureData, AlarmPressurePrediction>(model);
 
             var prediction = predictionFunc.Predict(newData);
+
 
             //ddHelper.SavePerfectAlarmDataset();
 
@@ -70,7 +72,9 @@ namespace temperaturepredictor
             //        var values = ddHelper.GetNewestWeatherforecastByStationId(Stations[i], j);
             //        prediction = predictionFunc.Predict(values.Item1);
             //        var date = values.Item2.Substring(6, 4) + "-" + values.Item2.ToString().Substring(3, 2) + "-" + (Convert.ToInt32(values.Item2.ToString().Substring(0, 2)) + j).ToString();
-            //        Console.WriteLine(values.Item2.Substring(6, 4) + "-" + (Convert.ToInt32(values.Item2.ToString().Substring(0, 2)) + j).ToString() + "-" + values.Item2.ToString().Substring(3, 2));
+            //        //Console.WriteLine(values.Item2.Substring(6, 4) + "-" + (Convert.ToInt32(values.Item2.ToString().Substring(0, 2)) + j).ToString() + "-" + values.Item2.ToString().Substring(3, 2));
+            //        //Console.WriteLine(prediction.PredictedAlarmPressure);
+            //        //Console.WriteLine(Convert.ToInt32(prediction.PredictedAlarmPressure));
             //        ddHelper.UploadNewestAlarmPrediction(date, Stations[i], Convert.ToInt32(prediction.PredictedAlarmPressure));
             //    }
             //}
@@ -78,14 +82,14 @@ namespace temperaturepredictor
             //Console.WriteLine("Færdig");
             //writer.WriteLine($"R^2 - {metrics.RSquared}" + ", " + $"RMSE - {metrics.RootMeanSquaredError}" + ", " + $"Prediction - {prediction.PredictedAlarmPressure}");
 
-            writer.Close();
+            //writer.Close();
             Console.WriteLine($"Prediction - {prediction.PredictedAlarmPressure}");
             //Console.ReadLine();
 
-            //var items = File.ReadAllLines(@"C:\Users\Asmus\Source\Repos\ADoessing\AlarmPressureEstimator\Csvs\LastSet4.csv").Skip(1).Select(line => line.Split(",")).Select(i => i);
+            //var items = File.ReadAllLines(@"C:\Users\Asmus\Source\Repos\ADoessing\AlarmPressureEstimator\Csvs\LastSetAll.csv").Skip(1).Select(line => line.Split(",")).Select(i => i);
             //foreach (string[] i in items)
             //{
-            //    ddHelper.fillDataSetTable(Convert.ToInt32(i[0]), Convert.ToInt32(i[1]), Convert.ToInt32(i[2]), Convert.ToDouble(i[3].Replace('.',',')), Convert.ToDouble(i[4].Replace('.',',')), Convert.ToDouble(i[5].Replace('.',',')), Convert.ToDouble(i[6].Replace('.',',')), Convert.ToDouble(i[7].Replace('.',',')));
+            //    ddHelper.fillDataSetTable(Convert.ToInt32(i[0]), Convert.ToInt32(i[1]), Convert.ToInt32(i[2]), Convert.ToDouble(i[3].Replace('.', ',')), Convert.ToDouble(i[4].Replace('.', ',')), Convert.ToDouble(i[5].Replace('.', ',')), Convert.ToDouble(i[6].Replace('.', ',')), Convert.ToDouble(i[7].Replace('.', ',')));
             //}
 
             //if (DateTime.Now.AddDays(1).Day == 1)
---
+++ SQL/SQLQuery2.sql
@@ -0,0 +1,20 @@
+﻿SELECT AVG(ALL case when Observations.ParameterId= 'humidity_past1h' then Observations.Value end) as Humidty, AVG(ALL case when Observations.ParameterId= 'pressure_at_sea' then Observations.Value end) as Pressure, AVG(ALL case when Observations.ParameterId= 'temp_mean_past1h' then Observations.Value end) as TempMean, Max(ALL case when Observations.ParameterId= 'temp_max_past1h' then Observations.Value end) as TempMax, MIN(ALL case when Observations.ParameterId= 'temp_min_past1h' then Observations.Value end) as TempMin from Observations where DateKey = '2020-11-17' and StationId = 06126
+
+SELECT SUM(AlarmItems) as AlarmItems from Subscriptions where StationId = 06126
+
+SELECT Count(ID) as Stores FROM Subscriptions where StationId = 06126
+
+SELECT SUM(AlarmCount) as Alarms FROM Alarms where DateKey = '2020-11-16' and IsValid = 1 and StationId = 06126
+
+SELECT Sum(AlarmCount) as Alarms, Datekey from Alarms where IsValid = 1 group by DateKey order by DateKey DESC
+
+SELECT * FROM Alarms where DateKey = '2020-11-16' AND IsValid = 1 order by AlarmCount
+
+SELECT * From Dataset Where Stores = 84
+
+SELECT * From ForecastsFromArima where StationId = 06123 and ForecastDay = 1
+
+Select * FROM ForecastsFromArima
+
+SELECT * FROM Observations Where Observed = (SELECT Max(Observed) FROM Observations) 
+
